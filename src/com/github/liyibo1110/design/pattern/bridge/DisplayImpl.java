c266034816e6ae2408ac9e8e4f1b4378a18732f8
liyibo1110 <liyibo1110@sina.com>
2020-07-02 01:18:57 +0800

    1.增加桥接模式

---
+++ src/com/github/liyibo1110/design/pattern/bridge/DisplayImpl.java
@@ -0,0 +1,8 @@
+package com.github.liyibo1110.design.pattern.bridge;
+
+public abstract class DisplayImpl {
+
+    public abstract void rawOpen();
+    public abstract void rawPrint();
+    public abstract void rawClose();
+}
---
+++ src/com/github/liyibo1110/design/pattern/bridge/StringDisplayImpl.java
@@ -0,0 +1,35 @@
+package com.github.liyibo1110.design.pattern.bridge;
+
+public class StringDisplayImpl extends DisplayImpl {
+
+    private String str;
+    private int width;
+
+    public StringDisplayImpl(String str) {
+        this.str = str;
+        this.width = str.getBytes().length;
+    }
+
+    @Override
+    public void rawOpen() {
+        printLine();
+    }
+
+    @Override
+    public void rawPrint() {
+        System.out.println("|" + str + "|");
+    }
+
+    @Override
+    public void rawClose() {
+        printLine();
+    }
+
+    private void printLine() {
+        System.out.print("+");
+        for (int i = 0; i < width; i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+    }
+}
